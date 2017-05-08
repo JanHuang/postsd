59aff12d98141fc2ee6782e285263d878bb03219
JanHuang <bboyjanhuang@gmail.com>
2017-05-08 22:53:31 +0800

    add delete methods

--- config/routes.php
+++ config/routes.php
@@ -2,14 +2,14 @@
 
 route()->group('/api', function () {
     route()->get('/posts', 'PostsController@findPosts');
-    route()->post('/posts', 'PostsController@createPost');
     route()->get('/posts/{id}', 'PostsController@findPost');
+    route()->post('/posts', 'PostsController@createPost');
     route()->patch('/posts/{id}', 'PostsController@patchPost');
     route()->delete('/posts/{id}', 'PostsController@deletePost');
     route()->get('/posts/tags/{tag}', 'TagPostsController@select');
     route()->get('/posts/users/{id}', 'UserPostsController@findUserPosts');
     route()->get('/posts/users/{id}/likes', 'UserPostsController@findUserLikesPosts');
+    route()->delete('/posts/users/{id}/likes', 'UserPostsController@findUserLikesPosts');
     route()->get('/posts/users/{id}/collects', 'UserPostsController@findUserCollectsPosts');
-    route()->post('/posts/relates', 'PostsShipController@create');
-    route()->delete('/posts/relates/{id}', 'PostsShipController@delete');
+    route()->delete('/posts/users/{id}/collects', 'UserPostsController@findUserCollectsPosts');
 });
--- src/Controller/UserPostsController.php
+++ src/Controller/UserPostsController.php
@@ -5,20 +5,34 @@ namespace Controller;
 
 use FastD\Http\ServerRequest;
 
+/**
+ * Class UserPostsController
+ * @package Controller
+ */
 class UserPostsController
 {
     public function findUserCollectsPosts(ServerRequest $request)
+    {
+        $userId = $request->getAttribute('id');
+        $likePosts = model('PostsShip')->findUsersPostsRelation($userId, 'collects');
+        return json($likePosts);
+    }
+
+    public function removeUserCollectsPosts()
+    {
+
+    }
+
+    public function findUserLikesPosts(ServerRequest $request)
     {
         $userId = $request->getAttribute('id');
         $likePosts = model('PostsShip')->findUsersPostsRelation($userId, 'likes');
         return json($likePosts);
     }
 
-    public function findUserLikesPosts(ServerRequest $request)
+    public function removeUserLikesPosts(ServerRequest $request)
     {
-        return json([
-            'foo' => 'bar'
-        ]);
+
     }
 
     public function findUserPosts(ServerRequest $request)
